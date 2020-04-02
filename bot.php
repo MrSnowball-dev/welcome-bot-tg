c96ebaf29d69f7d4fddc22796e5f2a6a8a8086e8
Daniil <igatorr19@gmail.com>
2020-04-02 23:49:02 +0300

    Revert back to MySQL

--- bot.php
+++ bot.php
@@ -26,174 +26,159 @@ if ($message == '/start') {
 }
 
 if ($message == '/init') {
-	$db = pg_connect("host=".$db_host." user=".$db_username." password=".$db_pass." dbname=".$db_schema);
-	
-	$stat = pg_connection_status($db);
-	if ($stat === PGSQL_CONNECTION_OK) echo "PGSQL connection is ok";
-		else echo "PGSQL failed to connect.\n".$stat;
+	$db = mysqli_connect($db_host, $db_username, $db_pass, $db_schema);
+	mysqli_set_charset($db, 'utf8mb4');
+	mysqli_query($db, "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+	if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		else echo "MySQL connect successful.\n";
 
-	if ($check = pg_query($db, 'select * from main')) {
-		$count = pg_num_rows($check);
+	if ($check = mysqli_query($db, 'select * from main')) {
+		$count = mysqli_num_rows($check);
 		echo "There is $count records in DB.\n\n";
-		pg_free_result($check);
+		mysqli_free_result($check);
 	}
 	
 	deleteMessage($chat_id, $message_id);
 	if ($chat_id > 0) {
 		sendMessage($chat_id, "Нельзя настроить приветственное сообщение в личном чате :)\nДобавь меня в группу и набери там /init для настройки!");
 	} else {
-		$query = pg_query($db, 'select chat_owner_user_id from main where chat_id=\''.$chat_id.'\'');
-		while ($sql = pg_fetch_object($query)) {
+		$query = mysqli_query($db, 'select chat_owner_user_id from main where chat_id='.$chat_id);
+		while ($sql = mysqli_fetch_object($query)) {
 			$owner_id = $sql->chat_owner_user_id;
 		}
 		if (($owner_id == $user_id) || ($owner_id === NULL)) {
-			$query = pg_query($db, 'select chat_id from main where chat_id=\''.$chat_id.'\'');
-			while ($sql = pg_fetch_object($query)) {
+			$query = mysqli_query($db, 'select chat_id from main where chat_id='.$chat_id);
+			while ($sql = mysqli_fetch_object($query)) {
 				$sql_chat_id = $sql->chat_id;
 			}
 			if ($sql_chat_id == $chat_id) {
-				$query = pg_query($db, 'select welcome_message_text from main where chat_id=\''.$chat_id.'\'');
-				while ($sql1 = pg_fetch_object($query)) {
+				$query = mysqli_query($db, 'select welcome_message_text from main where chat_id='.$chat_id);
+				while ($sql1 = mysqli_fetch_object($query)) {
 					$welcome_message = $sql1->welcome_message_text;
 				}
 				sendMessage($user_id, "Сообщение для чата _".$chat."_ уже настроено.\nТекущее сообщение:\n\n".$welcome_message."\n\nДля изменения приветственного сообщения напишите мне\n\n`/set ".$chat_id." <ваше сообщение>`\n^строку можно скопировать");
 			} else {
-				pg_query($db, "insert into main (chat_id, chat_owner_user_id) values ('".$chat_id."', '".$user_id."')");
+				mysqli_query($db, "insert into main (chat_id, chat_owner_user_id) values (".$chat_id.", ".$user_id.")");
 				sendMessage($user_id, "Вы включили приветственные сообщения для _".$chat."_!\nЧтобы задать своё приветствие, напишите мне\n\n`/set ".$chat_id." <ваше сообщение>`\n^строку можно скопировать\n\nВ дальнейшем, изменить приветствие для чата сможете только вы.\n\nПриветственные сообщения можно форматировать (пока MarkdownV2 не поддерживается). Для этого используйте следующий синтаксис:\n\n\_текст\_ - курсив\n\*текст\* - жирный\n\[текст](ссылка) - для вставки ссылки в форме текста\n\\n - перенос строки\n\nСсылки на пользователей через @ и хештеги # работают как обычно.\nДля поддержки пишите @mrsnowball");
 			}
 		} else {
 			sendMessage($chat_id, "У вас нет прав на изменение приветственных сообщений для этого чата!\nТекущий владелец доступен по [ссылке](tg://user?id=".$owner_id.").");
 		}
 	}
-	pg_free_result($sql);
-	pg_close($db);
+	mysqli_free_result($sql);
+	mysqli_close($db);
 }
 
 if ((is_int(stripos($message, '/set '))) && ($chat_id > 0)) {
-	$db = pg_connect("host=".$db_host." user=".$db_username." password=".$db_pass." dbname=".$db_schema);
-	
-	$stat = pg_connection_status($db);
-	if ($stat === PGSQL_CONNECTION_OK) echo "PGSQL connection is ok";
-		else echo "PGSQL failed to connect.\n".$stat;
+	$db = mysqli_connect($db_host, $db_username, $db_pass, $db_schema);
+	mysqli_set_charset($db, 'utf8mb4');
+	mysqli_query($db, "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+	if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		else echo "MySQL connect successful.\n";
 
-	if ($check = pg_query($db, 'select * from main')) {
-		$count = pg_num_rows($check);
+	if ($check = mysqli_query($db, 'select * from main')) {
+		$count = mysqli_num_rows($check);
 		echo "There is $count records in DB.\n\n";
-		pg_free_result($check);
+		mysqli_free_result($check);
 	}
-
 	$setup_array = explode(" ", substr($message, 5), 2);
 	$chat_to_setup = $setup_array[0];
 	$message_to_setup = $setup_array[1];
 
-	$query = pg_query($db, 'select chat_owner_user_id from main where chat_id=\''.$chat_to_setup.'\'');
-	while ($sql = pg_fetch_object($query)) {
+	$query = mysqli_query($db, 'select chat_owner_user_id from main where chat_id='.$chat_to_setup);
+	while ($sql = mysqli_fetch_object($query)) {
 		$owner_id = $sql->chat_owner_user_id;
 	}
 	if (($owner_id == $user_id) || ($owner_id === NULL)) {
-		pg_query($db, "update main set welcome_message_text='".$message_to_setup."' where chat_id='".$chat_to_setup."'");
+		mysqli_query($db, "update main set welcome_message_text='".$message_to_setup."' where chat_id=".$chat_to_setup);
 		sendMessage($chat_id, "Сообщение \n\n".$message_to_setup."\n\n для `".$chat_to_setup."` установлено.");
 	} else {
 		sendMessage($chat_id, "У вас нет прав на изменение приветственных сообщений для этого чата!\nТекущий владелец доступен по [ссылке](tg://user?id=".$owner.").");
 	}
-	pg_close($db);
+	mysqli_close($db);
 }
 
 if ($new_user !== 'new_user_empty') {
-	$db = pg_connect("host=".$db_host." user=".$db_username." password=".$db_pass." dbname=".$db_schema);
-	
-	$stat = pg_connection_status($db);
-	if ($stat === PGSQL_CONNECTION_OK) echo "PGSQL connection is ok";
-		else echo "PGSQL failed to connect.\n".$stat;
+	$db = mysqli_connect($db_host, $db_username, $db_pass, $db_schema);
+	mysqli_set_charset($db, 'utf8mb4');
+	mysqli_query($db, "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+	if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		else echo "MySQL connect successful.\n";
 
-	if ($check = pg_query($db, 'select * from main')) {
-		$count = pg_num_rows($check);
+	if ($check = mysqli_query($db, 'select * from main')) {
+		$count = mysqli_num_rows($check);
 		echo "There is $count records in DB.\n\n";
-		pg_free_result($check);
+		mysqli_free_result($check);
 	}
-
-	$query = pg_query($db, 'select chat_id from main where chat_id=\''.$chat_id.'\'');
-	while ($sql = pg_fetch_object($query)) {
+	$query = mysqli_query($db, 'select chat_id from main where chat_id='.$chat_id);
+	while ($sql = mysqli_fetch_object($query)) {
 		$sql_chat_id = $sql->chat_id;
 	}
 	
 	if ($sql_chat_id == $chat_id) {
-		$query = pg_query($db, 'select welcome_message_text from main where chat_id=\''.$chat_id.'\'');
-		while ($sql = pg_fetch_object($query)) {
+		$query = mysqli_query($db, 'select welcome_message_text from main where chat_id='.$chat_id);
+		while ($sql = mysqli_fetch_object($query)) {
 			$welcome_message = $sql->welcome_message_text;
 		}
-		pg_query($db, 'update main set welcome_count=welcome_count+1 where chat_id=\''.$chat_id.'\'');
+		mysqli_query($db, 'update main set welcome_count=welcome_count+1 where chat_id='.$chat_id);
 		sendWelcomeMessage($chat_id, $welcome_message, $message_id);
 	} else {
 		sendWelcomeMessage($chat_id, "Привет!", $message_id);
 	}
 
-	pg_free_result($sql);
-	pg_close($db);
+	mysqli_free_result($sql);
+	mysqli_close($db);
 }
 
 if (is_int(stripos($message, '/mysql'))) {
-	$db = pg_connect("host=".$db_host." user=".$db_username." password=".$db_pass." dbname=".$db_schema);
-	
-	$stat = pg_connection_status($db);
-	if ($stat === PGSQL_CONNECTION_OK) echo "PGSQL connection is ok";
-		else echo "PGSQL failed to connect.\n".$stat;
+	$db = mysqli_connect($db_host, $db_username, $db_pass, $db_schema);
+	mysqli_set_charset($db, 'utf8mb4');
+	mysqli_query($db, "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+	if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		else echo "MySQL connect successful.\n";
 
-	if ($check = pg_query($db, 'select * from main')) {
-		$count = pg_num_rows($check);
+	if ($check = mysqli_query($db, 'select * from main')) {
+		$count = mysqli_num_rows($check);
 		echo "There is $count records in DB.\n\n";
-		pg_free_result($check);
+		mysqli_free_result($check);
 	}
-
 	$query = substr($message, 7);
-	pg_query($db, $query);
+	mysqli_query($db, $query);
 	sendMessage($chat_id, "Доне\n".$query);
 }
 
 if ($message == '/meme') {
-	$db = pg_connect("host=".$db_host." user=".$db_username." password=".$db_pass." dbname=".$db_schema);
-	
-	$stat = pg_connection_status($db);
-	if ($stat === PGSQL_CONNECTION_OK) echo "PGSQL connection is ok";
-		else echo "PGSQL failed to connect.\n".$stat;
+	$db = mysqli_connect($db_host, $db_username, $db_pass, $db_schema);
+	mysqli_set_charset($db, 'utf8mb4');
+	mysqli_query($db, "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+	if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		else echo "MySQL connect successful.\n";
 
-	if ($check = pg_query($db, 'select * from main')) {
-		$count = pg_num_rows($check);
+	if ($check = mysqli_query($db, 'select * from main')) {
+		$count = mysqli_num_rows($check);
 		echo "There is $count records in DB.\n\n";
-		pg_free_result($check);
+		mysqli_free_result($check);
 	}
-	
-	$query = pg_query($db, 'select memecount from main where chat_id=\'-1001268103928\'');
-	while ($sql = pg_fetch_object($query)) {
-		$memecount = $sql->memecount;
-	}
-	pg_query($db, 'update main set memecount=memecount+1 where chat_id=\'-1001268103928\'');
 
-	pg_free_result($sql);
-	pg_close($db);
+	mysqli_query($db, 'update main set memecount=memecount+1 where chat_id=-1001268103928');
 }
 
 if ($message == '/memecount') {
-	$db = pg_connect("host=".$db_host." user=".$db_username." password=".$db_pass." dbname=".$db_schema);
-	
-	$stat = pg_connection_status($db);
-	if ($stat === PGSQL_CONNECTION_OK) echo "PGSQL connection is ok";
-		else echo "PGSQL failed to connect.\n".$stat;
+	$db = mysqli_connect($db_host, $db_username, $db_pass, $db_schema);
+	mysqli_set_charset($db, 'utf8mb4');
+	mysqli_query($db, "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+	if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		else echo "MySQL connect successful.\n";
 
-	if ($check = pg_query($db, 'select * from main')) {
-		$count = pg_num_rows($check);
+	if ($check = mysqli_query($db, 'select * from main')) {
+		$count = mysqli_num_rows($check);
 		echo "There is $count records in DB.\n\n";
-		pg_free_result($check);
+		mysqli_free_result($check);
 	}
-	
-	$query = pg_query($db, 'select memecount from main where chat_id=\'-1001268103928\'');
-	while ($sql = pg_fetch_object($query)) {
+	$query = mysqli_query($db, 'select memecount from main where chat_id=-1001268103928');
+	while ($sql = mysqli_fetch_object($query)) {
 		$memecount = $sql->memecount;
 	}
-
-	pg_free_result($sql);
-	pg_close($db);
-
 	sendMessage($chat_id, '/meme count (since 02.04.2020 0:00): '.$memecount.' memes.');
 }
 
--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $tg_bot_token = '1083015722:AAGoyePTsLkDIQ0rk1aPQh-3YEgXEebzKaw';
-$db_host = 'ec2-46-137-177-160.eu-west-1.compute.amazonaws.com';
-$db_username = 'tnrvfkowgjhvvi';
-$db_pass = '70fde27f2b06d800dd9ccfaa935a44a0ff1930fbe41f39026b3666a1d8b5db7b';
-$db_schema = 'dbpppp7irjosqo';
+$db_host = 'eu-cdbr-west-02.cleardb.net';
+$db_username = 'b54e6d9db71666';
+$db_pass = 'c8ef369d';
+$db_schema = 'heroku_08e631d10d72341';
 
 ?>
