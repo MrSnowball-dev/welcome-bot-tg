b101a96ce3629db9eede20fb8d190b2d03fb73fc
Daniil <igatorr19@gmail.com>
2020-04-25 02:20:27 +0300

    Migrate to different host

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 
 $tg_bot_token = '1083015722:AAGoyePTsLkDIQ0rk1aPQh-3YEgXEebzKaw';
-$db_host = 'eu-cdbr-west-02.cleardb.net';
-$db_username = 'b54e6d9db71666';
-$db_pass = 'c8ef369d';
-$db_schema = 'heroku_08e631d10d72341';
+
+$db_host = 'localhost';
+$db_username = 'i60485_admin';
+$db_pass = '183461Ad!!!';
+$db_schema = 'i60485_welcome_ng_bot';
 
 ?>
